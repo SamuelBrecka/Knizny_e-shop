c99311d2f7801f35705bfa2ccdb12a2a6b69e8e5
Samuel Brečka <brecka@stud.uniza.sk>
2025-01-31 22:28:29 +0100

    Pridanie hashovania hesiel

---
+++ PageKeep/Models/PasswordHasher.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PageKeep.Models.Entities;
+
+public static class PasswordHasher
+{
+    private static readonly PasswordHasher<UserAccount> _hasher = new PasswordHasher<UserAccount>();
+
+    public static string HashPassword(string password)
+    {
+        return _hasher.HashPassword(null, password);
+    }
+
+    public static bool VerifyPassword(string hashedPassword, string providedPassword)
+    {
+        var result = _hasher.VerifyHashedPassword(null, hashedPassword, providedPassword);
+        return result == PasswordVerificationResult.Success;
+    }
+}
