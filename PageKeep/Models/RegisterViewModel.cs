7fc516a46568cd6fb5e1bf5318d72e82834a7d97
Samuel Brečka <brecka@stud.uniza.sk>
2025-01-31 20:24:22 +0100

    Pridanie tlacidla a viewmodel na registraciu

---
+++ PageKeep/Models/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class RegisterViewModel
+{
+    [Required(ErrorMessage = "Používateľské meno je povinné.")]
+    public string UserName { get; set; }
+
+    [Required(ErrorMessage = "E-mail je povinný.")]
+    [EmailAddress(ErrorMessage = "Neplatný formát e-mailu.")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Heslo je povinné.")]
+    [MinLength(6, ErrorMessage = "Heslo musí mať aspoň 6 znakov.")]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "Potvrdenie hesla je povinné.")]
+    [Compare(nameof(Password), ErrorMessage = "Heslá sa musia zhodovať.")]
+    public string ConfirmPassword { get; set; }
+}
